a81794ecc249972c367a90a0d3e52511035621f8
David <151574925+Davedigital9@users.noreply.github.com>
2024-07-19 16:37:10 +0100

    Update data_tests.sql

--- assets/scripts/SQL/data_tests.sql
+++ assets/scripts/SQL/data_tests.sql
@@ -1,30 +1,60 @@
---ROW COUNT TEST
-SELECT COUNT(*) AS no_of_rows
-FROM view_uk_top_youtubers_2024
+/*
+Data quality tests 
+1. The data needs to be 100 records of YouTube channels (row count test)
+2. The data needs 4 fields (column count test)							
+3. The channel name column must be in string format, and the other columns must be numerical data types (data type check)
+4. Each record must be unique in the dataset (duplicate count check) 
 
---COLUMN COUNT TEST
-SELECT
-	COUNT(*) as column_count
+Row count - 100 
+Column count - 4
+Data types:	channel_name = VARCHAR
+		total_subscribers = INTEGER
+		total_views = INTEGER
+		total_videos = INTEGER
+Duplicate count = 0
+*/
+
+
+-- 1. Row count check 
+
+SELECT 
+	COUNT(*) as no_of_rows
+FROM 
+	view_uk_youtubers_2024 
+
+
+-- 2. Column count check 
+
+SELECT 
+	COUNT(*) as column_count 
 FROM 
 	INFORMATION_SCHEMA.COLUMNS
-WHERE
-	TABLE_NAME = 'view_uk_top_youtubers_2024'
+WHERE 
+	TABLE_NAME = 'view_uk_youtubers_2024'
 
---DATA TYPE TEST
-SELECT
+
+
+-- 3. Data type check 
+
+
+SELECT 
 	COLUMN_NAME,
 	DATA_TYPE
 FROM 
 	INFORMATION_SCHEMA.COLUMNS
-WHERE
-	TABLE_NAME = 'view_uk_top_youtubers_2024'
+WHERE 
+	TABLE_NAME = 'view_uk_youtubers_2024'
 
---DUPLICATE RECORDS TEST
-SELECT channel_name,
+
+
+-- 4. Duplicate records check
+
+SELECT 
+	channel_name,
 	COUNT(*) as duplicate_count
 FROM 
-	view_uk_top_youtubers_2024
+	view_uk_youtubers_2024
 GROUP BY 
-	channel_name
+	channel_name 
 HAVING 
-	COUNT(*) > 1
+	COUNT(*) > 1 
